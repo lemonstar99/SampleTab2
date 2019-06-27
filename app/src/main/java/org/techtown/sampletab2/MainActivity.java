e9fac49edd290d9b71e579f7008113ad36617410
lemonstar99 <44111674+lemonstar99@users.noreply.github.com>
2019-06-28 00:30:53 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/sampletab2/MainActivity.java
@@ -0,0 +1,88 @@
+package org.techtown.sampletab2;
+
+//import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    ArrayList<Integer> mImageIds = new ArrayList<>(Arrays.asList(
+            R.drawable.img1,R.drawable.img2, R.drawable.img3, R.drawable.img4,
+            R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8,
+            R.drawable.img9, R.drawable.img10, R.drawable.img11, R.drawable.img12,
+            R.drawable.img13, R.drawable.img14, R.drawable.img15, R.drawable.img16,
+            R.drawable.img17, R.drawable.img18, R.drawable.img19, R.drawable.img20
+    ));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tab1:
+                        Toast.makeText(getApplicationContext(), "첫 번째 탭 선택됨", Toast.LENGTH_LONG).show();
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, fragment1).commit();
+
+                        return true;
+                    case R.id.tab2:
+
+                        Toast.makeText(getApplicationContext(), "두 번째 탭 선택됨", Toast.LENGTH_LONG).show();
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, fragment2).commit();
+
+                        return true;
+                    case R.id.tab3:
+                        Toast.makeText(getApplicationContext(), "세 번째 탭 선택됨", Toast.LENGTH_LONG).show();
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, fragment3).commit();
+
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
